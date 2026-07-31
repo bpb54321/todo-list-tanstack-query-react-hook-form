79c65759810c0aeb316a997b2fcc9ced2d82c80f
Brian Blosser <bpb54321@gmail.com>
2026-07-30 20:18:19 -0400

    Update ignore patterns.

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,5 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "\\.visual\\.test\\.tsx$"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "\\.visual\\.test\\.ts(x)?$"],
 };
